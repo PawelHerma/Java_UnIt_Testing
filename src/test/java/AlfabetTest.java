a84f35bff258d089221a9ba7ec5bf5fcffdf1b3d
dmt-herma-pawel <dmt.pawel@lo5.bielsko.pl>
2023-03-31 12:06:08 +0200

    Alfabet - czwarty commit
    Trzeci test, ogólnikowy, nie działa

--- src/test/java/AlfabetTest.java
+++ src/test/java/AlfabetTest.java
@@ -25,4 +25,16 @@ public class AlfabetTest {
 
         assertEquals(oczekiwania, rzeczywistość);
     }
+    @Test
+    public void AlfabetTest3()
+    {
+        char[] znaczki = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        int[] oczekiwania = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26};
+        int[] rzeczywistości = new int[26];
+        for (int i = 0; i < 26; i++)
+        {
+            rzeczywistości[i] = Alfabet.numerek(znaczki[i]);
+            assertEquals(oczekiwania[i], rzeczywistości[i]);
+        }
+    }
 }
