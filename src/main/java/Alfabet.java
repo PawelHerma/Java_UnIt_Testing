415551de38ece53f0031a403ca346700de753701
dmt-herma-pawel <dmt.pawel@lo5.bielsko.pl>
2023-03-31 12:10:02 +0200

    Alfabet - piąty commit
    Pełny refaktoring, działa

--- src/main/java/Alfabet.java
+++ src/main/java/Alfabet.java
@@ -3,17 +3,6 @@ package main.java;
 public class Alfabet {
     public static int numerek(char znaczek)
     {
-        if (znaczek == 'a')
-        {
-            return 1;
-        }
-        else if (znaczek == 'w')
-        {
-            return 23;
-        }
-        else
-        {
-            return 0;
-        }
+        return znaczek - 'a' + 1;
     }
 }
